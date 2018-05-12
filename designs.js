1efa9c28fac70824b4ced2b0b6680a482feb686a
Mukul Sharma <mukulsharma11031998@gmail.com>
2018-05-13 00:05:36 +0530

    first commit

---
+++ designs.js
@@ -0,0 +1,43 @@
+var submit;
+
+submit = $('input[type="submit"]')
+
+submit.click(function(event) {
+    event.preventDefault();
+    makeGrid();
+});
+
+
+/**
+ * Make height x width grid
+ * @param {Number} height
+ * @param {Number} width
+ */
+function makeGrid() {
+    var pixelCanvas, position, gridHeight, gridWidth, row;
+    pixelCanvas = $('#pixel_canvas');
+    gridHeight = $('#input_height').val();
+    gridWidth = $('#input_width').val();
+
+    pixelCanvas.children().remove();
+
+    for (i = 0; i < gridHeight; i++) {
+        pixelCanvas.append('<tr></tr>');
+    }
+
+    row = $('tr');
+
+    for (j = 0; j < gridWidth; j++) {
+        row.append('<td></td>');
+    }
+
+    //checking the position and adding color to it
+    position = pixelCanvas.find('td');
+
+    position.click(function() {
+        var color;
+        color = $("#colorPicker").val();
+        $(this).attr('bgcolor', color);
+    });
+    
+}
